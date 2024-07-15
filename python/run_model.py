d08105907bc89cc6e540f4b0c1ca16981a101849
wasadmin <wasadmin@example.com>
2024-07-14 22:16:00 -0400

    model runner

---
+++ python/run_model.py
@@ -0,0 +1,17 @@
+from pymongo import MongoClient
+import pandas as pd
+import pickle
+from sklearn.neighbors import NearestNeighbors
+
+
+uri = 'mongodb://localhost:27017'
+cli = MongoClient(uri)
+db = cli['rei']
+
+loaded_model = pickle.load(open('NearestNeighbors.pkl', 'rb'))
+
+in_cart = db.cart.find()
+
+res = loaded_model.kneighbors(in_cart,return_distance=False)
+db.featured.delete_many({})
+db.featured.insert_many(res)
